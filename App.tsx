d8de19adb9bc9f9421af6429b309000f88b6b0d8
Dinesh <dinesh.k@brilworks.com>
2024-02-03 18:00:57 +0530

    Value component added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
+interface ValueProps {
+  label: string;
+  value: string;
+}
+const Value = ({ label, value }: ValueProps) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={{ flexDirection: "row", gap: 8 }}>
+        <Value label="Steps" value="1000" />
+        <Value label="Distance" value="10 km" />
+      </View>
+      <Value label="Flights Climbed" value="11" />
     </View>
   );
 }
@@ -13,8 +27,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "baseline",
+    justifyContent: "center",
+    padding: 24,
+  },
+  valueContainer: { marginRight: 50 },
+  label: { color: "white" },
+  value: {
+    fontSize: 34,
+    color: "#AFB3BE",
+    fontWeight: "500",
   },
 });

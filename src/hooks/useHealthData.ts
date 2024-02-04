ffc742714da5605465db49fb1f8f96f005284295
Dinesh <dinesh.k@brilworks.com>
2024-02-04 18:11:51 +0530

    Integrated Android Health Data

--- src/hooks/useHealthData.ts
+++ src/hooks/useHealthData.ts
@@ -5,6 +5,9 @@ import appleHealthKit, {
   HealthInputOptions,
   HealthValueOptions,
 } from "react-native-health";
+import { getSdkStatus, initialize, readRecords, requestPermission } from "react-native-health-connect";
+import { Permission } from "react-native-health-connect/lib/typescript/types";
+import { TimeRangeFilter } from "react-native-health-connect/lib/typescript/types/base.types";
 
 const permissions: HealthKitPermissions = {
   permissions: {
@@ -20,6 +23,11 @@ const permissions: HealthKitPermissions = {
     ],
   },
 };
+
+const permissionsAndroid: Permission[] = [{
+    "accessType": "read",
+    "recordType": "Steps",
+}]
 
 export const useHealthData = (date: Date) => {
   const [hasPermission, setHasPermission] = useState(false);
@@ -153,9 +161,71 @@ export const useHealthData = (date: Date) => {
     });
   }, [hasPermission]);
 
+  async function readAndroidHealthData() {
+     const sdkStatus = await getSdkStatus();
+     if (sdkStatus === 2) {
+        console.log('Health connect not initialised:- ', sdkStatus)
+        return;
+     }
+     
+    try {
+        const initialized = await initialize()
+
+        if (!initialized) {
+            console.log('Health connect not initialised:- ', initialized)
+        }
+
+        await requestPermission([
+            { accessType: 'read', recordType: 'Steps' },
+            { accessType: 'read', recordType: 'Distance' },
+            { accessType: 'read', recordType: 'FloorsClimbed' },
+          ]);
+      
+          const timeRangeFilter: TimeRangeFilter = {
+            operator: 'between',
+            startTime: new Date(date.setHours(0, 0, 0, 0)).toISOString(),
+            endTime: new Date(date.setHours(23, 59, 59, 999)).toISOString(),
+          };
+      
+          // Steps
+          const steps = await readRecords('Steps', { timeRangeFilter });
+          const totalSteps = steps.reduce((sum, cur) => sum + cur.count, 0);
+          setSteps(totalSteps);
+      
+          // Distance
+          const distance = await readRecords('Distance', { timeRangeFilter });
+          const totalDistance = distance.reduce(
+            (sum, cur) => sum + cur.distance.inMeters,
+            0
+          );
+          setDistance(totalDistance);
+      
+          // Floors climbed
+          const floorsClimbed = await readRecords('FloorsClimbed', {
+            timeRangeFilter,
+          });
+          const totalFloors = floorsClimbed.reduce((sum, cur) => sum + cur.floors, 0);
+          setFlightsClimbed(totalFloors);
+    } catch (error) {
+        console.log('Health connect not initialised:- ', error)
+    }
+    
+}
+
+
+  // Android: Health Connect
+  useEffect(() => {
+      if (Platform.OS !== "android") {
+          return;
+      }
+        
+      readAndroidHealthData();
+  }, [hasPermission]);
+
   return {
     steps,
     fightsClimbed,
     distance,
   };
 };
+
